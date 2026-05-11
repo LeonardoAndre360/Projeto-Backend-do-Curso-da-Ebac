4ba2f9f89875276b92c6757cb1b023e373497451
Leonardo <Leonardoandre3600@gmail.com>
2026-05-11 12:09:35 -0300

    Segunda Versão

--- main.py
+++ main.py
@@ -15,10 +15,21 @@
 # Delete
 
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 meus_livrozinhos = {}
+
+class Livro(BaseModel):
+    nome_livro: str
+    autor_livro: str
+    ano_livro: int
+
+app.get("/")
+def hello_world():
+    return {"Hello": "Word"}
 
 @app.get("/livros")
 def get_livros():
@@ -31,26 +42,22 @@ def get_livros():
 # nome do livro    
 # autor do  livro
 # ano de lançamento do livro
+
 @app.post("/adiciona")
-def post_livros(id_livro: int, nome_livro: str, autor_livro: str, ano_livro: int):
+def post_livros(id_livro: int, livro: Livro):
     if id_livro in meus_livrozinhos:
         raise HTTPException(status_code=400, detail="Este livro já existe!")
     else:
-        meus_livrozinhos[id_livro] = {"nome_livro": nome_livro, "autor_livro": autor_livro, "ano_livro": ano_livro}
+        meus_livrozinhos[id_livro] = livro.dict()
         return {"message": "O Livro foi criado com sucesso!"}
     
 @app.put("/atualiza/{id_livro}")
-def put_livros(id_livro: int, nome_livro: str, autor_livro: str, ano_livro: int):
+def put_livros(id_livro: int, livro: Livro):
     meu_livro = meus_livrozinhos.get(id_livro)
     if not meu_livro:
         raise HTTPException(status_code=404, detail="Este livro não foi encontrado!")
     else:
-        if nome_livro:
-            meu_livro["nome_livro"] = nome_livro
-        if autor_livro:
-            meu_livro["autor_livro"] = autor_livro
-        if ano_livro:
-            meu_livro["ano_livro"] = ano_livro
+        meu_livro[id_livro] = livro.dict()
 
         return {"message": "As informações do seu livro foram autalizadas com sucesso!"}
     
@@ -60,5 +67,4 @@ def delete_livros(id_livro: int):
         raise HTTPException(status_code=404, detail="Esse livro não foi encontrado!")
     else:
         del meus_livrozinhos[id_livro]
-
         return {"message": "Seu livro foi deletado com sucesso!"}
